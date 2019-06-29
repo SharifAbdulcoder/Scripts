2597b256d7e68883c3cbb351ef2d24443cce8ad7
SharifAbdulcoder <43179042+SharifAbdulcoder@users.noreply.github.com>
2019-06-29 15:16:40 -0500

    Create useradd.sh

---
+++ useradd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+read -p "grand or revoke access?: " gr
+if [ "$gr" == "grand" ]; then
+    read -p "New User or Existing user? (n, e): " user
+    if [ "$user" == "n" ]; then
+        read -p "Please enter username: " nuser
+        read -p "Please enter password: " npasswd
+        useradd $nuser
+        echo "npasswd" | passwd $nuser --stdin
+        mkdir /home/"$nuser"/.ssh
+        read -p "Please paste ssh key for the user $nuser: " nsshkey
+        echo "$nsshkey" >> /home/"$nuser"/.ssh/authorized_keys
+        echo "user $nuser has been granted"
+
+    elif [ "$user" == "e" ]; then
+      read -p "Please enter the username of existing user: " euser
+      mkdir /home/"$euser"/.ssh
+      read -p "Please paste ssh key for the user $euser: " esshkey
+      echo "$esshkey" >> /home/"$euser"/.ssh/authorized_keys
+      echo "user $euser has been granted access "
+    else
+      echo "invalid option"
+    fi
+
+elif [ "$gr" == "revoke" ]; then
+  read -p "Please enter the username of the user: " ruser
+  echo "BE AWARE the authorized_keys file will be emptied for the user $ruser"
+  echo "revoked" > /home/$ruser/.ssh/authorized_keys
+  echo "User $ruser has been revoked"
+
+else
+  echo "invalid option"
+
+fi
